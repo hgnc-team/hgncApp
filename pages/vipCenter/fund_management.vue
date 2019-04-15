ce959cac992596e4478e072178979862884c6677
chenshi <289905698@qq.com>
2019-04-15 16:34:47 +0800

    佣金管理页面制作

--- pages/vipCenter/fund_management.vue
+++ pages/vipCenter/fund_management.vue
@@ -1,41 +1,40 @@
 <template>
 	<view class="fundManagementPage">
-		<view class="table">
-			<view class="row uni-flex">
-				<view class="row-item uni-flex-item uni-center">
-					<view class="top">
-						9999
+		<view class="header">
+			<view class="balance uni-flex flex-center-center uni-column">
+				<view class="value uni-h3">
+					{{balance}}
+				</view>
+				<view class="name uni-text-small">
+					我的余额(元)
+				</view>
+			</view>
+			<view class="uni-flex uni-flex flex-center-center">
+				<view class="left uni-flex-item uni-column flex-center-center">
+					<view class="value uni-h4">
+						+{{newEarnings}}
 					</view>
-					<view class="bottom">
-						我的积分
+					<view class="name uni-text-small">
+						今日收益(元)
 					</view>
 				</view>
-				<view class="row-item uni-flex-item uni-center">
-					<view class="top">
-						9.88枚
+				<view class="right uni-flex-item uni-column flex-center-center">
+					<view class="value uni-h4">
+						+{{earnings}}
 					</view>
-					<view class="bottom">
-						今日释放M币
-					</view>
-				</view>
-				<view class="row-item uni-flex-item uni-center">
-					<view class="top">
-						9999.99枚
-					</view>
-					<view class="bottom">
-						已赚取
+					<view class="name uni-text-small">
+						已赚取(元)
 					</view>
 				</view>
 			</view>
-				
+		</view>
 	
-			<view class="list">
-				<uni-list>
-					<uni-list-item title="银行卡管理" @click="toBankCard" show-extra-icon="true" :extra-icon="{color: '#4cd964',size: '22',type: 'spinner'}"></uni-list-item>
-					<uni-list-item title="佣金管理" @click="toCommissionsManagement" show-extra-icon="true" :extra-icon="{color: '#4cd964',size: '22',type: 'spinner'}"></uni-list-item>
-					<uni-list-item title="提现记录" @click="toWithdrawalsRecord" show-extra-icon="true" :extra-icon="{color: '#4cd964',size: '22',type: 'spinner'}"></uni-list-item>
-				</uni-list>
-			</view>
+		<view class="list">
+			<uni-list>
+				<uni-list-item  :title="pageList[0].title"  @click="handleClick(0)"  :thumb="pageList[0].thumbUrl"></uni-list-item>
+				<uni-list-item  :title="pageList[1].title"  @click="handleClick(1)"  :thumb="pageList[1].thumbUrl"></uni-list-item>
+				<uni-list-item  :title="pageList[2].title"  @click="handleClick(2)"  :thumb="pageList[2].thumbUrl"></uni-list-item>
+			</uni-list>
 		</view>
 	</view>
 </template>
@@ -52,62 +51,90 @@
 		},
 		data() {
 			return {
-				
+				// 我的余额
+				balance: 0,
+				// 今日收益
+				newEarnings: 0,
+				// 已赚取
+				earnings: 0,
+				// 跳转其他功能页面列表
+				pageList:[
+					{
+						index: 0,
+						title:'银行卡管理',
+						thumbUrl: '/static/img/vipcenter/cards.png'
+					},
+					{
+						index: 1,
+						title:'佣金体现',
+						thumbUrl: '/static/img/vipcenter/yjtx.png'
+					},
+					{
+						index: 2,
+						title:'提现记录',
+						thumbUrl: '/static/img/vipcenter/txjl.png'
+					}
+				]
 			}
 		},
 		methods: {
-			// 银行卡管理
-			toBankCard(){
-				uni.navigateTo({
-					url: 'bank_card'
-				})
+			init(){
+				this.balance = "23,432,847.99";
+				this.newEarnings = 233.55;
+				this.earnings = 12349.23;
 			},
-			// 佣金管理
-			toCommissionsManagement(){
-				uni.navigateTo({
-					url: 'commissions_management'
-				})
-			},
-			// 提现记录
-			toWithdrawalsRecord(){
-				uni.navigateTo({
-					url: 'withdrawals_record'
-				})
-			},
+			// 点击跳转
+			handleClick(index) {
+				// 银行卡管理
+				if(index === 0) {
+					uni.navigateTo({
+						url: "/pages/vipCenter/bank_card"
+					})
+				// 佣金管理
+				} else if (index === 1) {
+					uni.navigateTo({
+						url: '/pages/vipCenter/commissions_management'
+					})
+				// 提现记录
+				} else if (index === 2) {
+					uni.navigateTo({
+						url: '/pages/vipCenter/withdrawals_record'
+					})
+					
+				}
+			}	
+		},
+		onLoad() {
+			this.init();
 		}
 	}
 </script>
 
 <style lang="scss">
+	page{
+		width: 100%;
+		height: 100%;
+		background-color: #f0f0f0;
+	}
 	.fundManagementPage{
-		.table{
+		.header{
 			width: 100%;
-			height: 420upx;
+			height: 380upx;
 			padding: 30upx;
 			box-sizing: border-box;
-			.row{
-				border-bottom: 2upx solid #999;
-				&:last-child{
-					border-bottom:none; 
-				}
-				.row-item{
-					margin: 20upx 0;
-					border-right: 1upx solid #999;
-					&:last-child{
-						border-right:none; 
-						margin-bottom: none;
-					}
-					.top{
-						color: #333;
-						font-weight: 600;
-					}
-					.bottom{
-						color: #999;
-						font-size:24upx;
-					}
-				}
+			background: linear-gradient( #242424 , #333);
+			margin-bottom: 30upx;
+			.balance{
+				width: 100%;
+				height: 150upx;
+				margin-bottom: 50upx;
 			}
-			// .row
+			.value{
+				color: #fff;
+			}
+			.name{
+				color: #999;
+			}
 		}
 	}
 </style>
