9582dbd9e22ad5d0656f44786fd5125d10b45c00
chenshi <289905698@qq.com>
2019-04-15 21:05:27 +0800

    提现详情页面制作

--- pages/vipCenter/withdrawals_record_detail.vue
+++ pages/vipCenter/withdrawals_record_detail.vue
@@ -1,14 +1,123 @@
 <template>
 	<view class="withdrawalsRecordDetailPage">
-		我的团队
+		<view class="item">
+			<view class="title text-color-gray">
+				提现金额
+			</view>
+			<view class="value">
+				￥{{money}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现金额
+			</view>
+			<view class="value">
+				￥{{money}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现方式
+			</view>
+			<view class="value">
+				{{mode}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现状态
+			</view>
+			<view class="value">
+				{{status}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				状态备注
+			</view>
+			<view class="value">
+				{{remark}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现创建时间
+			</view>
+			<view class="value">
+				{{creatTime}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现订单号
+			</view>
+			<view class="value">
+				{{orderId}}
+			</view>
+		</view>
+		<view class="item">
+			<view class="title text-color-gray">
+				提现流水号
+			</view>
+			<view class="value">
+				{{serialNo}}
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
-		
+		data() {
+			return {
+				// 提现金额
+				money: "",
+				// 提现方式
+				mode: "人工申请",
+				// 提现状态
+				status: "提现成功",
+				// 状态备注
+				remark:  "银行卡冻结",
+				// 提现创建时间
+				creatTime: "2019-04-15 17:44:55",
+				// 提现订单号
+				orderId: "45345346456456546546546456",
+				// 提现流水号
+				serialNo: "4398759843759437598437598437598435"
+				
+			}
+		},
+		computed:{
+			disabled(){
+				return this.card === "";
+			}
+		},
+		methods:{
+			init(){
+				this.name="奥斯卡"
+			},
+			// 添加银行卡
+			add(){
+				// uni.showLoading({})
+				uni.showToast({
+					title: "添加成功"
+				})
+			}
+		},
+		onLoad() {
+			this.init();
+		}
 	}
 </script>
 
 <style lang="scss">
+	page{
+		width: 100%;
+		height: 100%;
+		background-color: #fff;
+	}
+	.withdrawalsRecordDetailPage{
+		
+	}
 </style>
