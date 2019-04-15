eee5e7feb2625afecc9bdfc3e292d5d33c90204a
chenshi <289905698@qq.com>
2019-04-15 20:55:41 +0800

    提现记录页面制作

--- pages/vipCenter/withdrawals_record.vue
+++ pages/vipCenter/withdrawals_record.vue
@@ -3,28 +3,21 @@
 		<view class="list">
 			<view class="row-item uni-flex common-pa-30" v-for="(item, index) in dataList" :key="index" @tap="toDetail">
 				<view class="left uni-flex-item">
-					<view class="title uni-h5 uni-bold">
-						{{item.title}}
+					<view class="title uni-h5 uni-bold uni-flex uni-row">
+						<text class="uni-inline-item">{{item.title}}</text>
+						<text class="tag uni-inline-item" :class="{'success': item.status === '1'}">{{item.status === '1'? '提现成功':'提现失败'}}</text>
 					</view>
 					<view class="date uni-text-gray uni-text-small">
 						{{item.date}}
 					</view>
 				</view>
-				<view class="right uni-flex-item">
-					<view class="">
-						<view class="money">
-							{{item.money}}
-						</view>
-						<view class="status">
-							{{item.status}}
-						</view>
+				<view class="right uni-flex-item uni-flex">
+					<view class="money uni-flex-item uni-flex">
+						￥{{item.money}}
 					</view>
-					<view class="icon uni-flex-item">
-						<uni-icon type="rightarrow"></uni-icon>
+					<view class="icon uni-inline-item flex-center-center">
+						<uni-icon type="arrowright" size="20"></uni-icon>
 					</view>
-				</view>
-				<view class="icon">
-					
 				</view>
 			</view>
 		</view>
@@ -44,28 +37,78 @@
 				dataList: [{
 					title: "提现申请",
 					date: "2019-11-11 20:33:20",
-					money:"+$44.22",
-					status: "提现成功"
+					money:"44.22",
+					status: "1"
 				},{
 					title: "提现申请",
 					date: "2019-11-11 20:33:20",
-					money:"+$48.22",
-					status: "提现失败"
+					money:"48.22",
+					status: "0"
 				},{
 					title: "提现申请",
 					date: "2019-11-11 20:33:20",
-					money:"+$54.22",
-					status: "提现成功"
+					money:"54.22",
+					status: "1"
 				},{
 					title: "提现申请",
 					date: "2019-11-11 20:33:20",
-					money:"+$224.22",
-					status: "提现成功"
+					money:"224.22",
+					status: "1"
 				},{
 					title: "提现申请",
 					date: "2019-11-11 20:33:20",
-					money:"+$4994.22",
-					status: "提现成功"
+					money:"4994.22",
+					status: "0"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"44.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"48.22",
+					status: "0"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"54.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"224.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"4994.22",
+					status: "0"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"44.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"48.22",
+					status: "0"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"54.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"224.22",
+					status: "1"
+				},{
+					title: "提现申请",
+					date: "2019-11-11 20:33:20",
+					money:"4994.22",
+					status: "0"
 				}]
 			}
 		},
@@ -87,6 +130,32 @@
 				height: 120upx;
 				border-bottom: 1upx solid #f0f0f0;
 				box-sizing: border-box;
+				.title{
+					align-items: center;
+					.tag{
+						height: 36upx;
+						line-height: 36upx;
+						border-radius: 2upx;
+						color: #f7695b;
+						background-color: #ffedeb;
+						font-size: 20upx;
+						padding: 0 10upx;
+						box-sizing: border-box;
+						margin-left: 10upx; 
+						&.success{
+							color: #20c380;
+							background-color: #e8f9f1;
+						}
+					}
+				}
+				.money{
+					align-items: center;
+					justify-content: flex-end;
+				}
+				.icon{
+					width: 70upx;
+					color: #999;
+				}
 			}
 		}
 	}
