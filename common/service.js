fb2558ed2a65adfa4261946a2ca97d113148d5d3
chenshi2017 <289906598@qq.com>
2019-05-24 00:09:12 +0800

    优化获取订单列表接口

--- common/service.js
+++ common/service.js
@@ -353,7 +353,7 @@ const createOrder = function(params) {
   **/
 const getOrderList = function(params) {
 	let data = {
-		userId: vm.$store.state.userId,
+		userId: params.userId || vm.$store.state.userId,
 		// userId: "e8b46f10-43c8-11e9-9de7-55194d563065",
 		status: params.status,
 		page: params.page,
